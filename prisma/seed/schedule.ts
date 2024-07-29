4a401d37e319d54dca8edf9db9fafcbfab336b02
Diego Andress Salas <diegoandress717@gmail.com>
2024-07-29 17:58:55 -0500

    feat: agregamos supabase como db

--- prisma/seed/schedule.ts
+++ prisma/seed/schedule.ts
@@ -1,40 +1,18 @@
 // prisma/seed/schedule.ts
 import { Prisma } from "@prisma/client";
 
-export const demoSchedules: Prisma.ScheduleCreateInput[] = [
+export const demoSchedules: Prisma.ScheduleCreateManyInput[] = [
   {
     serviceId: 1,
-    date: "2024-07-04",
     dayOfWeek: "MONDAY",
-    scheduleTimes: {
-      create: [
-        { timeId: 1 }, // 09:00:00 am
-        { timeId: 2 }, // 10:00:00 am
-        { timeId: 3 }, // 11:00:00 am
-      ],
-    },
   },
   {
     serviceId: 2,
-    date: "2024-07-05",
     dayOfWeek: "TUESDAY",
-    scheduleTimes: {
-      create: [
-        { timeId: 4 }, // 12:00:00 pm
-        { timeId: 5 }, // 01:00:00 pm
-      ],
-    },
   },
   {
     serviceId: 3,
-    date: "2024-07-06",
     dayOfWeek: "WEDNESDAY",
-    scheduleTimes: {
-      create: [
-        { timeId: 6 }, // 02:00:00 pm
-        { timeId: 7 }, // 03:00:00 pm
-        { timeId: 8 }, // 04:00:00 pm
-      ],
-    },
+    
   },
 ];
